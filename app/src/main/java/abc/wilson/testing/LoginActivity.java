370d303f0b26cc31f51b1d95030f83a69bfc008e
Wilson <chanwilson1993@gmail.com>
2016-03-28 09:50:46 +0800

    First commit

---
+++ app/src/main/java/abc/wilson/testing/LoginActivity.java
@@ -0,0 +1,51 @@
+package abc.wilson.testing;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText userNameInput;
+    EditText passwordInput;
+    Button loginBtn;
+    String userInput;
+
+
+
+
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        loginBtn = (Button)findViewById(R.id.logInBtn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               userInput = userNameInput.getText().toString();
+            }
+        });
+
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+    }
+
+}
